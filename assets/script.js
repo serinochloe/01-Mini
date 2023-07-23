aa9292b256129363814082d8d114f43dd488629f
serinochloe <serinochloe@gmail.com>
2023-07-23 14:54:11 -0600

    adding challenge's html, js, and css files

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,11 @@
+var question = document.getElementById("question");
+var opt1 = document.getElementById("opt1");
+var opt2 = document.getElementById("opt2");
+var opt3 = document.getElementById("opt3");
+var title = document.getElementById("title");
+var welcome = document.getElementById("welcome");
+var playButton = document.getElementById("play");
 
+playButton.addEventListener("click", function() {
+    
+})
